7c9ea2b812b0d8918e7a52ccdfe95c54a1919d4c
Peter-SB <peterboughton11@live.co.uk>
2026-04-27 07:48:49 +0100

    gh page

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',        // enables static HTML export
+  basePath: '/eve-portfolio-website', // your GitHub repo name
   typescript: {
     ignoreBuildErrors: true,
   },
