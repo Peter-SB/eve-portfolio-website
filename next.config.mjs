5954214d8bf611035eb28628b5bbd238cc28c30f
Peter-SB <peterboughton11@live.co.uk>
2026-06-02 17:51:46 +0100

    update base path for new url

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',        // enables static HTML export
-  basePath: '/eve-portfolio-website', // your GitHub repo name
   env: {
-    NEXT_PUBLIC_BASE_PATH: '/eve-portfolio-website',
+    NEXT_PUBLIC_BASE_PATH: '',
   },
   typescript: {
     ignoreBuildErrors: true,
